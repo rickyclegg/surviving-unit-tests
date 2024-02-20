a728e3953da24d9c1990c3fdbddd6dd4f91b5cc9
Ricky Clegg <ricky.clegg@transferwise.com>
2024-02-20 08:37:52 +0000

    feat: add test shell for tdd

---
+++ src/test/java/PokedexTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PokedexTest {
+
+    @Test
+    void testGetByNameExists() {
+
+    }
+
+//    @Test
+//    void testGetByNameNotExists() {
+//
+//    }
+//
+//    @Test
+//    void testGetByNameCaseInsensitive() {
+//
+//    }
+}
